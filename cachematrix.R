f35cb90a93d28d8d092a47e89f935f73f3c919e1
Twinkle-soni <48448000+Twinkle-soni@users.noreply.github.com>
2019-03-13 15:19:41 +0530

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,40 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL
+    
+    setMatrix <- function(y) {
+        x <<- y
+        invMatrix <<- NULL
+    }
+    
+    getMatrix <- function() x                              
+    setInverse <- function(inverse) invMatrix <<- inverse  
+    getInverse <- function() invMatrix                     
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse, getInverse = getInverse)
+
+}
+
+
+
+cacheSolve <- function(x, ...) {
+    
+    
+    invMatrix <- x$getInverse()
+    if(!is.null(invMatrix)) {
+        message("Getting Cached Invertible Matrix")   
+        return(invMatrix)
+    }
+    
+    
+    MatrixData <- x$getMatrix()
+    invMatrix <- solve(MatrixData, ...)
+    x$setInverse(invMatrix) 
+    return(invMatrix)
+    
+    
+}
